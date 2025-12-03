b7843cbf901410416cc92e93597033b4d7f08c83
David4735 <davidkagian55@gmail.com>
2025-12-03 15:43:22 +0300

    PZ_6_2.py

--- PZ_6/PZ_6_2.py
+++ PZ_6/PZ_6_2.py
@@ -4,18 +4,18 @@ import random;
 try:
     n = random.randint(5, 15)
     lst = [random.randint(1, 100) for _ in range(n)]
-    print(f"\n2. N={n}")
-    print(f"   Список: {lst}")
+    print("N =", n)
+    print("Список: ", lst)
 
-    max_min = None
+    max = 0
     for i in range(1, n-1):
         if lst[i] < lst[i-1] and lst[i] < lst[i+1]:
-            if max_min is None or lst[i] > max_min:
-                max_min = lst[i]
+            if max == 0 or lst[i] > max:
+                max = lst[i]
 
-    if max_min is not None:
-        print(f"   Максимальный локальный минимум: {max_min}")
+    if max != 0:
+        print("Максимальный локальный минимум:", max)
     else:
-        print("   Нет локальных минимумов")
+        print("Нет локальных минимумов")
 except ValueError:
     print("Ошибка! Введено не число!")  # если ввели что то кроме чисел выходит ошибка
