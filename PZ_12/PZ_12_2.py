e13e151f31d2c8874b4aba20c40a278ed64fe596
David4735 <davidkagian55@gmail.com>
2026-05-30 11:24:34 +0300

    PZ_12_2.py

--- PZ_12/PZ_12_2.py
+++ PZ_12/PZ_12_2.py
@@ -4,12 +4,10 @@ import random
 m1 = [[random.randint(0, 20) for _ in range(5)] for _ in range(4)]
 
 print("Исходная матрица:")
-list(map(lambda r: print(r), m1))
+for r in m1:
+    print(r)
 
 m2 = list(map(lambda row: list(map(lambda x: 0 if x > 10 else x, row)), m1))
-
-print("Результат:")
-list(map(lambda r: print(r), m2))
 
 print("Результат:")
 for r in m2:
