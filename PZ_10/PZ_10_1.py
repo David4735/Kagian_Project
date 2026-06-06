c816615f1364e9fc4366bf74b55fc535b410bf46
David4735 <davidkagian55@gmail.com>
2026-06-06 10:39:43 +0300

    PZ_10_1.py

--- PZ_10/PZ_10_1.py
+++ PZ_10/PZ_10_1.py
@@ -1,87 +1,88 @@
-#Средствами языка Python сформировать текстовый файл (.txt), содержащий последовательность из целых положительных и отрицательных чисел. 
-# Сформировать новый текстовый файл (.txt) следующего вида, предварительно выполнив требуемую обработку элементов:
+# Вариант 6.
+# Задание 1.
+# Средствами языка Python сформировать текстовый файл (.txt), содержащий
+# последовательность из целых положительных и отрицательных чисел. Сформировать новый
+# текстовый файл (.txt) следующего вида, предварительно выполнив требуемую обработку элементов:
+# Исходные данные:
+# Количество элементов:
+# Произведение элементов:
+# Повторяющиеся элементы:
+# Количество повторяющихся элементов:
+# Элементы больше 5 увеличены в два раза:
 
-#Исходные данные:
-#Количество элементов:
-#Произведение элементов:
-#Повторяющиеся элементы:
-#Количество повторяющихся элементов:
-#Элементы больше 5 увеличены в два раза:
+import random
 
-import random;
+n = []
+a = random.randint(1, 11)
 
-n = 15
-d = ''
+for i in range(a):
+    n.append(random.randint(-99, 100))
 
-for i in range(n):
-    a = random.randint(-20, 20)
-    d = d + str(a)
-    if i < n - 1:
-        d = d + ''
-b = open('chisla.txt', 'w')
-b.write(d)
-b.close()
+f1 = open('data_1.txt', 'w')
+for i in n:
+    f1.write(str(i) + ' ')
+f1.close()
 
-b = open('chisla.txt', 'r')
-s = b.read()
-b.close()
+f1 = open('data_1.txt')
+k = f1.read().split()
 
-s = s.split()
-for i in range(len(s)):
-    s[i] = int(s[i])
+for i in range(len(k)):
+    k[i] = int(k[i])
 
+f1.close()
+
+c = len(k)
 p = 1
-for i in range(len(s)):
-    p = p * s[i]
-pov = []
-for i in range(len(s)):
-    l = 0
-    for j in range(len(s)):
-        if s[i] == s[j]:
-            l = l + 1
-    if l > 1:
-        o = 0
-        for k in range(len(pov)):
-            if s[i] == pov[k]:
-                o = 1
-        if o == 0:
-            pov.append(s[i])
+for i in k:
+    p = p * i
 
-kpov = len(pov)
+d = []
+for i in range(len(k)):
+    t = 0
+    for j in range(len(k)):
+        if k[i] == k[j]:
+            t += 1
+    if t > 1:
+        u = 0
+        for l in range(len(d)):
+            if k[i] == d[l]:
+                u = 1
+        if u == 0:
+            d.append(k[i])
 
-bolshe = []
-for i in range(len(s)):
-    if s[i] > 5:
-        bolshe.append(s[i] * 2)
+b = []
+for i in k:
+    if i > 5:
+        b.append(i * 2)
     else:
-        bolshe.append(s[i])
+        b.append(i)
 
-r = open('result.txt', 'w')
-r.write('Исходные данные: ')
-r.write(d)
-r.write('\n')
-r.write('Количество элементов')
-r.write(str(len(s)))
-r.write('\n')
-r.write('Произведение элементов: ')
-r.write(str(p))
-r.write('\n')
-r.write('Повторяющееся элементы: ')
-if len(pov) > 0:
-    for i in range(len(pov)):
-        r.write(str(pov[i]))
-        if i < len(pov) - 1:
-            r.write(' ')
-else:
-    r.write('нет')
-r.write('\n')
-r.write('Количество повторяющихся элементов: ')
-r.write(str(kpov))
-r.write('\n')
-r.write('Элементы более 5 увеличены в два раза: ')
-for i in range(len(bolshe)):
-    r.write(str(bolshe[i]))
-    if i < len(bolshe) - 1:
-        r.write(' ')
-r.close()
-print('1 задание готово')
+f2 = open('data_2.txt', 'w', encoding='utf-16')
+
+f2.write('Исходные данные: ')
+f2.write(str(k))
+f2.write('\n')
+
+f2.write('Количество элементов: ')
+f2.write(str(c))
+f2.write('\n')
+
+f2.write('Произведение элементов: ')
+f2.write(str(p))
+f2.write('\n')
+
+f2.write('Повторяющиеся элементы: ')
+f2.write(str(d))
+f2.write('\n')
+
+f2.write('Количество повторяющихся элементов: ')
+f2.write(str(len(d)))
+f2.write('\n')
+
+f2.write('Элементы больше 5 увеличены в два раза: ')
+f2.write(str(b))
+
+f2.close()
+
+for i in open('data_2.txt', encoding='utf-16'):
+    print(i, end='')
