fdaf0b9b1c098aa945c22cd1db7d370d9f9623a1
David4735 <davidkagian55@gmail.com>
2026-06-06 10:40:26 +0300

    PZ_10_2.py

--- PZ_10/PZ_10_2.py
+++ PZ_10/PZ_10_2.py
@@ -1,38 +1,38 @@
-#Из предложенного текстового файла (text18-6.txt) вывести на экран его содержимое,
-#количество пробельных символов. Сформировать новый файл, в который поместить текст
-#в стихотворной форме предварительно заменив все знаки пунктуации на знак «!».
-a = open('text10-2.txt', 'r', encoding='UTF-8')
-l = a.readlines()
-a.close()
+# Вариант 6.
+# Задание 2.
+# Из предложенного текстового файла (text18-6.txt) вывести на экран его содержимое,
+# количество пробельных символов. Сформировать новый файл, в который поместить текст
+# в стихотворной форме предварительно заменив все знаки пунктуации на знак «!».
 
-print('Содержимое файла:')
-print('-' * 30)
-for i in range(len(l)):
-    print(l[i], end='')
-print('-' * 30)
+p = 0
 
-prob = 0
-for i in range(len(l)):
-    for j in range(len(l[i])):
-        if l[i][j] == ' ' or l[i][j] == '\t' or l[i][j] == '\n':
-            prob = prob + 1
+for i in open('text18-6.txt', encoding='utf-16'):
+    print(i, end='')
+    for j in i:
+        if j == ' ' or j == '\n':
+            p += 1
 
-print('Количество пробельных символов:', prob)
+print(end='\n\n')
+print('Количество пробельных символов: ', p, end='\n\n')
 
-znaki = ['.', ',', '!', '?', ';', ':', '-', '(', ')', '"', "'"]
+f1 = open('text18-6.txt', encoding='utf-16')
+l = f1.readlines()
+f1.close()
 
-for i in range(len(l)):
-    novaya_stroka = ''
-    for j in range(len(l[i])):
-        if l[i][j] in znaki:
-            novaya_stroka = novaya_stroka + '!'
+z = ['.', ',', '!', '?', ';', ':', '-', '(', ')', '"', "'"]
+
+f2 = open('new_text18-6.txt', 'w', encoding='utf-16')
+
+for i in l:
+    s = ''
+    for j in i:
+        if j in z:
+            s += '!'
         else:
-            novaya_stroka = novaya_stroka + l[i][j]
-    l[i] = novaya_stroka
+            s += j
+    f2.write(s)
 
-w = open('text10-2_new.txt', 'w', encoding='UTF-8')
-for i in range(len(l)):
-    w.write(l[i])
-w.close()
+f2.close()
 
-print('2 задание готово! Смотри text10-2_new.txt')
+for i in open('new_text18-6.txt', encoding='utf-16'):
+    print(i, end='')
